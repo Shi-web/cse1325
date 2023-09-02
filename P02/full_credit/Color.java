28ada0f3dc4a56ea8d33f246e8743e235d8e3753
Khushi Gauli <kxg1688@mavs.uta.edu>
2023-09-02 17:17:23 -0500

    P02 full_credit

---
+++ P02/full_credit/Color.java
@@ -0,0 +1,1 @@
+public enum Color{PURPLE, INDIGO, CYAN, CRIMSON}
---
+++ P02/full_credit/TestLine.java
@@ -0,0 +1,19 @@
+public class TestLine
+{
+	public static void main(String[] args)
+	{
+		Line line1 = new Line(8.987,5.767,2.567,7.898,Color.PURPLE);
+		System.out.println(line1);
+		
+		Line line2 = new Line(10.987,25.767,7.567,7.898, Color.INDIGO);
+		System.out.println(line2);
+		
+		Line line3 = new Line(6.987,5.767,4.567,7.898,Color.CYAN);
+		System.out.println(line3);
+		
+		Line line4 = new Line(11.987,2.767,5.567,9.898,Color.CRIMSON);
+		System.out.println(line4);
+		
+		
+	}
+}
