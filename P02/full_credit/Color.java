6feedcb6e65de2f66ae9600d6c55cf6edf5e5db6
Khushi Gauli <kxg1688@mavs.uta.edu>
2023-09-03 01:58:02 -0500

    bonus

--- P02/full_credit/Color.java
+++ P02/full_credit/Color.java
@@ -1,1 +1,28 @@
-public enum Color{PURPLE, INDIGO, CYAN, CRIMSON}
+public enum Color
+{
+	PURPLE (128,0,128), 
+	INDIGO (75,00,130), 
+	CYAN (0,255,255), 
+	CRIMSON (220,20,60);
+	
+	private int r;
+	private int g;
+	private int b;
+	
+	private Color(int r, int g, int b)
+	{
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	
+	public String toString()
+	{
+		return this.name() + " (0x" +
+           Integer.toHexString(r | 0x100).substring(1).toUpperCase() +
+           Integer.toHexString(g | 0x100).substring(1).toUpperCase() +
+           Integer.toHexString(b | 0x100).substring(1).toUpperCase() +
+           ") ";
+	
+	}
+}
