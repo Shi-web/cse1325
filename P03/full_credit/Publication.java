122837e29862b607a12c2338516ccf1cb8b3b377
Khushi Gauli <kxg1688@mavs.uta.edu>
2023-09-09 18:01:36 -0500

    full_credit

--- P03/full_credit/Publication.java
+++ P03/full_credit/Publication.java
@@ -1,27 +1,27 @@
-import java.time.LocalDate
+import java.time.LocalDate;
 
 public class Publication{
 	
 	private String title;
-	private author;
+	private String author;
 	private int copyright;
 	private String loanedTo;
-	private LOcalDate dueDate;
+	private LocalDate dueDate;
 
 	public Publication(String title, String author, int copyright){
 		this.title = title;
 		this.author = author;
-		
+		this.copyright = copyright;
 		if (this.copyright < 1900 || this.copyright > LocalDate.now().getYear())
 			throw new IllegalArgumentException("Copyright year must be between 1900 and " + LocalDate.now().getYear());
-		this.copyright = copyright;
+		
 		this.loanedTo = null;
 		this.dueDate = null;
 	}
 	
 	public void checkOut(String patron){
 		this.loanedTo = patron;
-		this.dueDate = LOcalDate.now().plusDays(14);
+		this.dueDate = LocalDate.now().plusDays(14);
 	}
 	
 	@Override
